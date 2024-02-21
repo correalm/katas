39628d332c370bcc8848b72209fc885090c91d89
correalm <luismigueldasilva.99@gmail.com>
2024-02-21 08:50:42 -0300

    rgb kata

--- __tests__/rgb.test.js
+++ __tests__/rgb.test.js
@@ -1,7 +1,11 @@
+import rgb from '../katas/rgb'
+
 describe('rgb function', () => {
   test('returns the correct values', () => {
     expect(rgb(255,255,255)).toBe('FFFFFF')
     expect(rgb(255,255,300)).toBe('FFFFFF')
     expect(rgb(148,0,211)).toBe('9400D3')
+    expect(rgb(248, 214, 15)).toBe('F8D6FF')
+    expect(rgb(10, 275, 268)).toBe('AAFFFF')
   })
 })
--- katas/rgb.js
+++ katas/rgb.js
@@ -1,2 +1,26 @@
-function rgb(r,g,b) {
+/**
+  * @param {number} r
+  * @param {number} g
+  * @param {number} b
+  * @returns {string}
+*/
+export default function rgb(r,g,b) {
+   function validateHexValue(hexValue) {
+    if (hexValue.length < 2) {
+      hexValue += hexValue
+    }
+
+    return hexValue
+  }
+
+  function numberToHex(number) {
+    let result = number.toString(16)
+
+    if (number > 255) result = 'FF'
+    if (number < 0) result = '00'
+
+    return validateHexValue(result)
+   }
+
+  return `${numberToHex(r)}${numberToHex(g)}${numberToHex(b)}`.toUpperCase()
 }
