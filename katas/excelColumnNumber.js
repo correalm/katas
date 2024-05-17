947294467cc7cfc1ad83e057ee5f1402bc7c1693
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-05-17 07:28:13 -0300

    refac: remove temp variables

--- katas/excelColumnNumber.js
+++ katas/excelColumnNumber.js
@@ -1,11 +1,10 @@
+const ASCII_UPPERCASE_LETTERS_START_VALUE = 64
+
 export default function excelColumnNumber(title) {
   let result = 0
 
-  for (let i = title.length - 1; i >= 0; i--) {
-    const value = title.charCodeAt(i) - 64
-    const exponencial = title.length - 1 - i
-
-    result += Math.pow(26, exponencial) * value
+  for (let i = title.length; i > 0; i--) {
+    result += Math.pow(26, title.length - i) * (title.charCodeAt(i - 1) - ASCII_UPPERCASE_LETTERS_START_VALUE)
   }
 
   return result
