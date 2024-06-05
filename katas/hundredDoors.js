5b0f6a58c79ba38505fc1bccf63e0e8f1775c5a6
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-06-05 08:31:39 -0300

    refac: remove unnecessary validation

--- katas/hundredDoors.js
+++ katas/hundredDoors.js
@@ -2,16 +2,8 @@ export default function hundredDoors(doors) {
   const internal = [...doors]
 
   for (let i = 0; i < doors.length; i++) {
-    doors.forEach((door, index) => {
-      if (i + 1 === 1) {
-        internal[index] = changeDoorState(door)
-        return
-      }
-
-
-      if ((index + 1)%(i + 1) === 0) {
-        internal[index] = changeDoorState(internal[index])
-      }
+    doors.forEach((_, index) => {
+      if ((index + 1)%(i + 1) === 0) internal[index] = changeDoorState(internal[index])
     })
   }
 
