a7a0b09cd15078134420397efcb52fd6b875a279
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-06-05 20:53:57 -0300

    spec: add test for slow version of hundred doors

--- __tests__/hundredDoors.test.js
+++ __tests__/hundredDoors.test.js
@@ -1,8 +1,11 @@
-import hundredDoors from '../katas/hundredDoors'
+import hundredDoors from "../katas/hundredDoors/hundredDoors"
+import hundredDoorsLowPerf from "../katas/hundredDoors/hundredDoorsLowPerf"
 
 describe('hundredDoors function', () => {
   test('returns the correct values', () => {
+
     expect(hundredDoors(buildDoors(100))).toEqual(ANSWER)
+    expect(hundredDoorsLowPerf(buildDoors(100))).toEqual(ANSWER)
   })
 })
 
